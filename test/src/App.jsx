51e05000260f2e814efc4f67eded2c27aff253fb
adipatel12 <adipatel323233@gmail.com>
2026-08-10 23:59:35 +0530

    add initail files

---
+++ test/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        <div className="top"></div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyUzGyrbLN56FWSnUt92r-__KKc5dTij2xTp0SfcXsOg&s" alt="" />
+        <button>save </button>
+        <div className="center"></div>
+        <h3>amazon <span>5 days ago </span></h3>
+        <h2>senior full stack developer</h2>
+        <div>
+          <h4>part time</h4>
+          <h4>senior level </h4>
+
+        </div>
+
+
+      </div>
+      <div className="bottom">
+        <div>
+          <div>
+            <h3>$220/hour</h3>
+            <p> new york, USA </p>
+
+          </div>
+          <button>apply now </button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
